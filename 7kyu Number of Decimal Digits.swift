c507fe52828811844d7fdd57e90bd6361f42dbb8
ギルバ <61905056+SuperMario1212@users.noreply.github.com>
2021-06-22 14:50:53 +0800

    Added solutions

--- 7kyu Number of Decimal Digits.swift
+++ 7kyu Number of Decimal Digits.swift
@@ -1,1 +1,18 @@
+/* --> Instuctions <--
+Determine the total number of digits in the integer (n>=0) given as input to the function. 
+For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. 
+Be careful to avoid overflows/underflows.
 
+All inputs will be valid.
+*/
+
+// <My Solution>
+func digits(num n: UInt64) -> Int {
+  var myString = String(n)
+  return myString.count
+}
+
+// <Others Solution>
+func digits(num n: UInt64) -> Int {
+    return String(n).characters.count
+}
