d107bd905cbe3e00983fa13a8bd2da4e00edceeb
ギルバ <61905056+SuperMario1212@users.noreply.github.com>
2021-06-24 13:21:28 +0800

    Added solution

--- 6kyu New Cashier Does Not Know About Space or Shift.swift
+++ 6kyu New Cashier Does Not Know About Space or Shift.swift
@@ -1,1 +1,41 @@
+/* --> Instructions <--
 
+Some new cashiers started to work at your restaurant.
+
+They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+
+All the orders they create look something like this:
+
+"milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+
+Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+
+"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+The kitchen staff expect the items to be in the same order as they appear in the menu.
+
+The menu items are fairly simple, there is no overlap in the names of the items:
+
+1. Burger
+2. Fries
+3. Chicken
+4. Pizza
+5. Sandwich
+6. Onionrings
+7. Milkshake
+8. Coke
+*/
+
+
+// --> Best Solution <-- 
+import Foundation
+
+func getOrder(_ input: String) -> String {
+    let menu = ["Burger", "Fries", "Chicken", "Pizza", "Sandwich", "Onionrings", "Milkshake", "Coke"]
+    
+    return menu.reduce([]) {
+      $0 + Array(repeating: $1, count: input.components(separatedBy: $1.lowercased()).count - 1)
+    }.joined(separator: " ")
+}
